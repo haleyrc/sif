4b2381d8fd4f1d31983b9400d8516826ced4386a
Ryan Haley <ryanhaley@frazer.biz>
2020-03-26 20:53:27 -0400

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Greeting() {
+    return <h1>Hello, world!</h1>
+}
+
+export { Greeting }
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
